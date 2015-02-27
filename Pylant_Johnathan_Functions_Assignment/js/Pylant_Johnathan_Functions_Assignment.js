cf073972d409277fa523220e598e5f21f85a7fcf
John Pylant <johnathan.pylant@gmail.com>
2015-02-27 12:02:38 -0500

    Declaring variables

--- Pylant_Johnathan_Functions_Assignment/js/Pylant_Johnathan_Functions_Assignment.js
+++ Pylant_Johnathan_Functions_Assignment/js/Pylant_Johnathan_Functions_Assignment.js
@@ -1,1 +1,20 @@
 //NAME: Pylant, Johnathan DATE: February 26, 2015 ASSIGNMENT: Functions
+
+//DECLARATION OF VARIABLES
+var FLlotto;
+var PBlotto;
+var FLNrepeat;
+var PBNrepeat;
+var FLdWinningNumbers;
+var PBdWinningNumbers;
+var FL1;
+var FL2;
+var FL3;
+var FL4;
+var FL5;
+var PB1;
+var PB2;
+var PB3;
+var PB4;
+var PB5;
+var PB6;
