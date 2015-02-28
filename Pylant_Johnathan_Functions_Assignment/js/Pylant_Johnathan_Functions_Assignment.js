b8e1ffa1e1d4ee1974279d4ad3886a820cca253b
John Pylant <johnathan.pylant@gmail.com>
2015-02-28 09:46:53 -0500

    FLNlotto-Prompt Validation Function.
    The function that validates the input of the initial prompt.

--- Pylant_Johnathan_Functions_Assignment/js/Pylant_Johnathan_Functions_Assignment.js
+++ Pylant_Johnathan_Functions_Assignment/js/Pylant_Johnathan_Functions_Assignment.js
@@ -25,3 +25,22 @@ var PB5;
 var PB6;
 
 //FUNCTIONS
+//Function validating input for FL lotto prompt
+function FLNvalidation(){
+    //Establishes a start a count to go toward break-code below
+    var FLNrepeat = 1;
+    //If the FLlotto field is left bland, the below error will run and prompt the user for input a few more times.
+    while (FLlotto === "") {
+        FLlotto = prompt("ERROR0: Do not leave this blank. \nI cannot help you if you do not tell me what you want." +
+        " \nWould you like to see the WINNING Florida Lottery Numbers?");
+        //Adds to the established counter above
+        FLNrepeat = FLNrepeat + 1;
+            //When the user fails to fill out the prompt enough times, the below error message is presented and the
+            //loop is broken.
+            if (FLNrepeat === 4) {
+                console.log("ERROR1: I'm not clairvoyant, but if you play the lotto like you answer questions, I am " +
+                "pretty sure you didn't win.");
+                break;
+            }
+    }
+}
